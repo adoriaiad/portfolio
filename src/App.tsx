1d0951ff408842f778180a46117b432d6b35f24c
angelita.doria <angelita.doria@iad2.it>
2023-02-08 13:13:04 +0100

    Firs page under construction

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './assets/img/_logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} alt="logo" />
+        - sito in costruzione -
+      </header>
+    </div>
+  );
+}
+
+export default App;
