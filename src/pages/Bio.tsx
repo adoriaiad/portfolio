a112d14be03ab6e6b2e91626aa1ec4f31b8ceea6
angelita.doria <angelita.doria@iad2.it>
2023-02-10 17:45:16 +0100

    delete tmp file

--- src/pages/Bio.tsx
+++ src/pages/Bio.tsx
@@ -17,13 +17,17 @@ function Bio() {
             align="justify"
             style={{ margin: "60px" }}
           >
-            Angelita Doria è nata in Italia, attualmente residente in Belgio.
+            Angelita Doria è nata e cresciuta in Italia.
+            Ha conseguito studi scientifici e si è laureata in Biologia,
+            ma ha sviluppato interesse per l'informatica e
+            lavora da sempre in questo ambito.
+            Nel corso degli anni ha frequentato un corso
+            di illustrazione naturalistica ad acquerello e ha studiato da autodidatta
+            le tecniche della pittura e del disegno soprattutto della figura umana.
             Appassionata al disegno e all'illustrazione fin da bambina, curiosa,
-            sensibile alla natura e alla sua bellezza. Si è laureata in
-            Biologia, ha sempre lavorato come web developer, eseguito
-            commissioni artistiche nel tempo libero e per passione. Attualmente
+            sensibile alla natura e alla sua bellezza. Attualmente
             interessata al campo dell'illustrazione per bambini ha in corso un
-            progetto che spera sarà il primo di tanti altri. Ama la musica e
+            progetto che spera diventi il primo di tanti altri. Ama la musica e
             suona a livello amatoriale il sassofono, l'armonica blues, l'ukulele
             e frequenta la scuola di pianoforte moderno.
           </Typography>
