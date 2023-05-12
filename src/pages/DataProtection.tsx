14478cd21f796d604a872ce0cf1e2463ec5107e1
Angelita Doria <angelita.doria@external.iad2.it>
2023-05-12 15:11:23 +0200

    Add english text

--- src/pages/DataProtection.tsx
+++ src/pages/DataProtection.tsx
@@ -1,17 +1,20 @@
-import Link from "@mui/material/Link";
-import Typography from "@mui/material/Typography";
-import { Container } from "@mui/system";
-import React from "react";
+import { Divider } from '@mui/material';
+import Link from '@mui/material/Link';
+import Typography from '@mui/material/Typography';
+import { Container } from '@mui/system';
+import React from 'react';
+import { useTranslation } from 'react-i18next';
 
 function DataProtection() {
+  const { t } = useTranslation();
   return (
-    <Container style={{ margin: "0px" }}>
+    <Container style={{ margin: '0px' }}>
       <Typography variant="h4" align="center">
-        Protezione dei dati
+        {t('DATAPROTECTION')}
       </Typography>
-      <Typography variant="body2" color="text.secondary" align="justify">
-        Il proprietario di questo sito Web è Angelita Doria, con sede legale in
-        Belgio, Grimbergen. <br />I dati personali forniti dall'utente
+      <Typography variant="body2" color="text.secondary" align="justify" style={{marginBottom: '10px'}}>
+        Il proprietario di questo sito Web è Angelita Doria.
+        <br />I dati personali forniti dall'utente
         attraverso la pagina Web e tramite un modulo o e-mail, sono riservati e
         faranno parte dei file di proprietà di Angelita Doria al fine di
         rispondere alle richieste di informazioni che potrebbero aver
@@ -41,8 +44,45 @@ function DataProtection() {
         registrati a nome di Angelita Doria, in conformità e nel rispetto della
         normativa vigente in materia di Protezione dei Dati Personali. In ogni
         momento, puoi manifestare la tua volontà di NON ricevere alcun tipo di
-        pubblicità comunicandolo via email:{" "}
-        <Link href={"mailto:info@angelitadoria.com"}>
+        pubblicità comunicandolo via email:{' '}
+        <Link href={'mailto:info@angelitadoria.com'}>
+          info@angelitadoria.com
+        </Link>
+        .
+      </Typography>
+      <Divider />
+      <Typography variant="body2" color="text.secondary" align="justify" style={{marginTop: '10px'}}>
+        The owner of this website is Angelita Doria.<br />
+        The personal data provided by the user through the web page and through
+        a form or email, are confidential and will be part of the files owned by
+        Angelita Doria in order to respond to requests for information they may
+        have expressed interest.
+        <br /> The data entered in the forms by user are assumed to be true,
+        accurate, complete and up-to-date.
+        <br /> You will be solely responsible for any damage or loss, direct or
+        indirect, caused to third parties by entering, through the form, of
+        false, inaccurate, incomplete and outdated data. Manager of the archive
+        undertakes to respect the obligation of secrecy in this regard to the
+        personal data contained in its archives and derived from it website.
+        <br /> Likewise, data's owner is recognized for exercise his rights of
+        access, rectification, cancellation and opposition, which can be
+        exercised by email to info@angelitadoria.com, providing a copy of the
+        DNI as provided for in the Instruction of General Data Protection
+        Regulation (RGPD) - Regulation (EU 2016/679).
+        <br />
+        Angelita Doria reserves the right to change its policy confidentiality
+        and data protection at any time on the basis of current and future
+        applicable regulations.
+        <br /> The user authorizes Angelita Doria to obtain or acquire data
+        relating to the connection and, possibly, the use of files or a user's
+        disk verification record or "cookie", only a operational, security and
+        statistical purposes. , data that in a possible treatment the
+        information will be dissociated from his personal data, registered in
+        the name of Angelita Doria, in accordance with and in compliance with
+        the current legislation on the protection of personal data. In each
+        moment, you can express your will NOT to receive any kind of advertising
+        by communicating it via email:{' '}
+        <Link href={'mailto:info@angelitadoria.com'}>
           info@angelitadoria.com
         </Link>
         .
