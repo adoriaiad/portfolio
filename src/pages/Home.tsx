fa1e5943f4785820662924d3febcae73c0f85e89
Angelita Doria <doria.angelita@esternibisp.com>
2025-02-22 17:42:08 +0100

    modificata immagine home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Grid } from '@mui/material';
 
-import home from './../assets/img/homepage3.jpg';
+import home from './../assets/img/casa.png';
 
 function Home() {
   return (
