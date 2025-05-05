22bf0f546fd981c6c805b5096bfc98502075dfa2
Angelita Doria <doria.angelita@esternibisp.com>
2025-05-05 16:00:46 +0200

    aggiunta bozza

--- src/pages/robottoni/Robot.tsx
+++ src/pages/robottoni/Robot.tsx
@@ -3,6 +3,7 @@ import Grid from '@mui/material/Grid';
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 import { useNavigate } from 'react-router-dom';
 import tavola1 from './../../assets/img/books/cover/02.jpg';
+import bozza from './../../assets/img/books/cover/02-bozza.jpg';
 
 function Robot() {
     const navigation = useNavigate();
@@ -21,6 +22,11 @@ function Robot() {
           src={tavola1}
           alt=""
         />
+         <img
+            className="book-image"
+            src={bozza}
+            alt=""
+          />
         <Button
           variant="outlined"
           startIcon={<ArrowBackIosIcon />}
