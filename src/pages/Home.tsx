00d2ebbbe23798323c618ee7fada84fc4f9e43cd
Angelita Doria <doria.angelita@esternibisp.com>
2025-08-01 14:28:38 +0200

    Aggiunto pulsante

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,63 @@
 import React from 'react';
-import { Grid } from '@mui/material';
-
+import { Button, Grid, Typography } from '@mui/material';
 import home from './../assets/img/casa.png';
+import { useTranslation } from 'react-i18next';
+import { useBook } from '../data/useBook';
+import { useNavigate } from 'react-router-dom';
 
 function Home() {
+  const { t } = useTranslation();
+  const { books } = useBook();
+  const navigation = useNavigate();
   return (
     <Grid
       container
-      spacing={2}
-      style={{ display: 'flex', justifyContent: 'center' }}
+      spacing={1}
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        flexDirection: 'column',
+        alignItems: 'center',
+      }}
     >
       <img src={home} alt={''} className="Home" />
+      <Typography
+        variant={'h4'}
+        color={'text.secondary'}
+        sx={{ flexShrink: 0 }}
+        style={{ fontFamily: 'Yomogi', fontWeight: 'regular' }}
+      >
+        {t('BOOK.BOOKS')}
+      </Typography>
+      <Grid
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          flexDirection: 'row',
+          alignItems: 'center',
+        }}
+      >
+        {books.map((item, index) => (
+          <div className={'BookCard'} key={index}>
+            <div key={`img-${index}`} className="ComicCard">
+              <img
+                style={{
+                  height: '200px',
+                  padding: '4px',
+                }}
+                src={item.src}
+                key={index}
+                alt=""
+              />
+            </div>
+          </div>
+        ))}
+      </Grid>
+      <Grid>
+        <Button variant="outlined" onClick={() => navigation('/books')}>
+          {t('MENU.DISCOVER')}
+        </Button>
+      </Grid>
     </Grid>
   );
 }
