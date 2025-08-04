e5ccf0a11a5841d556f213a110663d31e93468f9
Angelita Doria <doria.angelita@esternibisp.com>
2025-08-04 11:26:51 +0200

    modificata pagina music

--- src/pages/Musica.tsx
+++ src/pages/Musica.tsx
@@ -12,22 +12,22 @@ function Musica(){
         <Grid
         container
         spacing={2}
-        style={{ display: 'flex', justifyContent: 'center', flexDirection: 'column' }}
+        style={{ display: 'flex', justifyContent: 'center', flexDirection: 'column', alignItems: 'center', marginTop: '10px' }}
       >
-        <Button onClick={() => playAudio('/audio/00-soundtrack.mp3')}>MUSICA</Button>
-        <Button onClick={() => playAudio('/audio/01-violino.mp3')}>VIOLINO</Button>
-        <Button onClick={() => playAudio('/audio/02-tromba.mp3')}>TROMBETTA</Button>
-        <Button onClick={() => playAudio('/audio/03-flauto.mp3')}>FLAUTO</Button>
-        <Button onClick={() => playAudio('/audio/04-chitarra.mp3')}>CHITARRA</Button>
-        <Button onClick={() => playAudio('/audio/05-piano.mp3')}>PIANOFORTE</Button>
-        <Button onClick={() => playAudio('/audio/06-tuba.mp3')}>TUBA</Button>
-        <Button onClick={() => playAudio('/audio/07-clarinetto.mp3')}>CLARINETTO</Button>
-        <Button onClick={() => playAudio('/audio/08-arpa.mp3')}>ARPA</Button>
-        <Button onClick={() => playAudio('/audio/09-violoncello.mp3')}>VIOLONCELLO</Button>
-        <Button onClick={() => playAudio('/audio/10-percussioni.mp3')}>PERCUSSIONI</Button>
-        <Button onClick={() => playAudio('/audio/11-baraonda.mp3')}>MIX</Button>
-        <Button onClick={() => playAudio('/audio/12-finale.mp3')}>FINALE</Button>
-        <Button onClick={() => playAudio('/audio/13-applausi.mp3')}>APPLAUSI</Button>
+        <Grid style={{marginBottom: '4px' }}><Button onClick={() => playAudio('/audio/00-soundtrack.mp3')} variant="outlined" className="Musicbtn">MUSICA</Button></Grid>
+        <Grid style={{marginBottom: '4px' }}><Button onClick={() => playAudio('/audio/01-violino.mp3')} variant="outlined" className="Musicbtn">VIOLINO</Button></Grid>
+        <Grid style={{marginBottom: '4px' }}><Button onClick={() => playAudio('/audio/02-tromba.mp3')} variant="outlined" className="Musicbtn">TROMBETTA</Button></Grid>
+        <Grid style={{marginBottom: '4px' }}><Button onClick={() => playAudio('/audio/03-flauto.mp3')} variant="outlined" className="Musicbtn">FLAUTO</Button></Grid>
+        <Grid style={{marginBottom: '4px' }}><Button onClick={() => playAudio('/audio/04-chitarra.mp3')} variant="outlined" className="Musicbtn">CHITARRA</Button></Grid>
+        <Grid style={{marginBottom: '4px' }}><Button onClick={() => playAudio('/audio/05-piano.mp3')} variant="outlined" className="Musicbtn">PIANOFORTE</Button></Grid>
+        <Grid style={{marginBottom: '4px' }}><Button onClick={() => playAudio('/audio/06-tuba.mp3')} variant="outlined" className="Musicbtn">TUBA</Button></Grid>
+        <Grid style={{marginBottom: '4px' }}><Button onClick={() => playAudio('/audio/07-clarinetto.mp3')} variant="outlined" className="Musicbtn">CLARINETTO</Button></Grid>
+        <Grid style={{marginBottom: '4px' }}><Button onClick={() => playAudio('/audio/08-arpa.mp3')} variant="outlined" className="Musicbtn">ARPA</Button></Grid>
+        <Grid style={{marginBottom: '4px' }}><Button onClick={() => playAudio('/audio/09-violoncello.mp3')} variant="outlined" className="Musicbtn">VIOLONCELLO</Button></Grid>
+        <Grid style={{marginBottom: '4px' }}><Button onClick={() => playAudio('/audio/10-percussioni.mp3')} variant="outlined" className="Musicbtn">PERCUSSIONI</Button></Grid>
+        <Grid style={{marginBottom: '4px' }}><Button onClick={() => playAudio('/audio/11-baraonda.mp3')} variant="outlined" className="Musicbtn">MIX</Button></Grid>
+        <Grid style={{marginBottom: '4px' }}><Button onClick={() => playAudio('/audio/12-finale.mp3')} variant="outlined" className="Musicbtn">FINALE</Button></Grid>
+        <Grid style={{marginBottom: '4px' }}><Button onClick={() => playAudio('/audio/13-applausi.mp3')} variant="outlined" className="Musicbtn">APPLAUSI</Button></Grid>
       </Grid>
     );
 }
